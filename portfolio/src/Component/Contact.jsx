20c7eb4757e3ff57ec2673d2be4c54f13eb6a7db
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-07-15 22:13:41 +0530

    update

--- portfolio/src/Component/Contact.jsx
+++ portfolio/src/Component/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
 
       console.log('📤 Sending data:', payload);
 
-      const response = await fetch('https://portfolio-oksl.onrender.com/api/contact', {
+      const response = await fetch('https://portfolio-h6a2.onrender.com/api/contact', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
