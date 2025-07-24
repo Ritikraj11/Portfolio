90e196637839653310741c3cf3c0452f1c185ef7
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-07-24 18:35:46 +0530

    Debugging

--- portfolio/src/Component/Myservices.jsx
+++ portfolio/src/Component/Myservices.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './MyServices.css';
+import './Myservices.css';
 import { FaLaptopCode, FaLayerGroup, FaProjectDiagram } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
